b3f8edf575340aab034e307d2baead7bc85f5deb
Jesse Gabriel <43253337+Jesse-Gabriel@users.noreply.github.com>
2021-04-08 12:46:18 -0400

    post routing

--- server/routes/post.js
+++ server/routes/post.js
@@ -115,9 +115,47 @@ router.put('/like', requireLogin, (req, res) => {
         })
 })
 
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull: { likes: req.user._id }
+    }, {
+        new: true
+    })
+        .populate("likes", "_id username profPic")
+        .populate("comments.postedBy", "_id username profPic")
+        .populate("postedBy", "_id username profPic")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err });
+            }
+            else {
+                res.json(result);
+            }
+        })
+})
 
-
-
+router.put('/comment', requireLogin, (req, res) => {
+    const comment = {
+        text: req.body.text,
+        postedBy: req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: { comments: comment }
+    }, {
+        new: true
+    })
+        .populate("likes", "_id username profPic")
+        .populate("comments.postedBy", "_id username profPic")
+        .populate("postedBy", "_id username profPic")
+        .exec((err, result) => {
+            if (err) {
+                return res.status(422).json({ error: err });
+            }
+            else {
+                res.json(result);
+            }
+        })
+})
 
 router.delete('/deletepost/:postId', requireLogin, (req, res) => {
     Post.findOne({ _id: req.params.postId })
