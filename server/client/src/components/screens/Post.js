31a560c1daaa4b371a65300680983d795d5fd607
Wayne Pud <waynepud@gmail.com>
2021-04-10 22:37:13 -0400

    Revert "Merge pull request #143 from COMP231W21-G3/richard"
    This reverts commit a234b2749d4e3d3df930283bd2c830d643026502, reversing
    changes made to 06b91595ddec504298e78c3fd04f529d29946b95.

--- server/client/src/components/screens/Post.js
+++ server/client/src/components/screens/Post.js
@@ -1,46 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
 import CarouselSlider from '../CarouselSlider';
-import HomePostOptions from '../HomePostOptions';
-import PreLoader from '../Preloader';
-import { UserContext } from '../../App';
-import M from 'materialize-css';
-import CommentsModal from '../CommentsModal';
-import LikesModal from '../LikesModal';
-import { Link, useHistory, useParams } from 'react-router-dom';
-
-const makeComment = (text, postId) => {
-    fetch('/comment', {
-        method: "put",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + localStorage.getItem("jwt")
-        },
-        body: JSON.stringify({
-            postId,
-            text
-        })
-    })
-        .then(res => res.json())
-        .then(result => {
-            console.log(result);
-            let newData = null;
-            if (data._id === result._id) {
-                newData = result;
-            }
-            else {
-                newData = data;
-            }
-            setData(newData);
-
-            var commentinput = document.getElementsByClassName(`cominput-${postId}`)[0]
-            commentinput.value = "";
-            commentinput.blur();
-
-        }).catch(err => {
-            console.log(err);
-        })
-}
-
 import PreLoader from '../Preloader';
 import { UserContext } from '../../App';
 import M from 'materialize-css';
