6dda818784e6c7be5ce6c2218fca06d5904a293d
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2021-04-13 21:50:46 -0300

    Code Unfollow User button / Code route to edit a post /

--- server/client/src/components/HomePostOptions.js
+++ server/client/src/components/HomePostOptions.js
@@ -4,10 +4,29 @@ import { Link } from 'react-router-dom';
 const HomePostOptions = ({ item, state, deletePost }) => {
     return (
         <div className="collection">
+            <Link to={`/post/${item._id}`} className="collection-item">View Post</Link>
+            {item.postedBy._id == state._id ?
                 <div>
+                    <Link to={`/profile`} className="collection-item">
+                        View Profile</Link>
+                    <Link to={`/editpost/${item._id}`} className="collection-item">Edit Post</Link>
                     <a href="#!" className="collection-item red-text" onClick={() => deletePost(item._id)}>Delete Post</a>
-                    <a href="#!" className="collection-item">Edit Post</a>
                 </div>
+                : <div>
+                    <Link to={state.username===item.postedBy.username?'/profile':`/profile/${item.postedBy.username}`} className="collection-item">
+                        View Profile</Link>
+                    {
+                        !state.following || (state.following && !state.following.some(user=>user._id===item.postedBy._id)) ?
+                            <a href="#!" className="collection-item blue-text"
+                                onClick={() => followUser(item.postedBy._id)}>
+                                Follow</a>
+                            :
+                            <a href="#!" className="collection-item red-text"
+                                onClick={() => unfollowUser(item.postedBy._id)}>
+                                Unfollow</a>
+                    }
+                </div>
+            }
         </div>
     )
 }
