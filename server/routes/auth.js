7631d629f577c82976e9a48d9eb2a29c4bab2c3f
elloquent <richardelridge78@gmail.com>
2021-04-15 20:58:04 -0500

    Code function to fetch request to leave a group chat
    Code function to fetch request to leave a group chat

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -75,34 +75,58 @@ router.post('/signup', (req, res) => {
             })
 });
 
-router.post('/signin', (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(422).json({ error: "Please add email and password" });
+router.post('/signup', (req, res) => {
+    const { username, email, password, profPic } = req.body;
+    if (!email || !password || !username) {
+        return res.status(422).json({ error: "Please add all the fields" });
     }
     User.findOne({ email: email })
-        .populate("followers", "_id username profPic")
-        .populate("following", "_id username profPic")
-        .then(savedUser => {
-            if (!savedUser) {
-                return res.status(422).json({ error: "Invalid email or password" });
-            }
-            bcrypt.compare(password, savedUser.password)
-                .then(doMatch => {
-                    if (doMatch) {
-                        //res.json({ message: "Successfully signed in" });
-                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET); //generate token based on user id
-                        const { _id, username, email, followers, profPic, following, hobbies } = savedUser;
-                        res.json({ token, user: { _id, username, email, profPic, followers, following, hobbies } });
-                    }
-                    else {
-                        return res.status(422).json({ error: "Invalid email or password" });
-                    }
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        })
+        .then(
+            (savedUser) => {
+                if (savedUser) {
+                    return res.status(422).json({ error: "Email already existed" });
+                }
+                User.findOne({ username: username })
+                    .then(
+                        (savedUser) => {
+                            if (savedUser) {
+                                return res.status(422).json({ error: "Username already existed" });
+                            }
+                            bcrypt.hash(password, 12) //12 is level of security. default is 10
+                                .then(hashedPassword => {
+                                    const user = new User({
+                                        email,
+                                        username,
+                                        password: hashedPassword,
+                                        profPic
+                                    })
+                                    user.save()
+                                        .then(user => {
+                                            transporter.sendMail({
+                                                to: user.email,
+                                                from: "phongstagramherokuapp@gmail.com",
+                                                subject: "Signup Success!",
+                                                html: "<h1>Welcome to Phongstagram!</h1> <img style='width:300px;' src='cid:unique@kreata.ee'/>",
+                                                attachments: [{
+                                                    filename: 'phongstagram_thumbnail.png',
+                                                    path: 'https://res.cloudinary.com/phongcloudinary/image/upload/v1602559179/phongstagram_thumb_qqiduz.png',
+                                                    cid: 'unique@kreata.ee' //same cid value as in the html img src
+                                                }]
+                                            })
+
+                                            res.json({ message: "Saved successfully" });
+                                        })
+                                        .catch(err => {
+                                            console.log(err);
+                                        })
+                                })
+                        })
+                    .catch(err => {
+                        console.log(err);
+                    })
+            }).catch(err => {
+                console.log(err);
+            })
 });
 
 module.exports = router;
